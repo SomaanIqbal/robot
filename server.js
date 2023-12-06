b42d1c2eeb151be873d26c6c4fd06b613a120212
SomaanIqbal <isomaan@gmail.com>
2023-12-05 22:36:00 -0800

    preparing for render

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// init db client
+const client = require('./db/client');
+client.connect();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Router: /api
+app.use('/api', require('./api'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
